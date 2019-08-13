a250c19d36a6558344c76edc2b5ae03b335bdeb1
LiMingjie_Office <limingjie_mail@163.com>
2019-08-13 12:02:34 +0800

    init project

---
+++ iOS-AutoTest-Demo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iOS-AutoTest-Demo
+//
+//  Created by LiMingjie on 2019/8/13.
+//  Copyright © 2019 LMJ. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iOS-AutoTest-Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iOS-AutoTest-Demo
+//
+//  Created by LiMingjie on 2019/8/13.
+//  Copyright © 2019 LMJ. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
