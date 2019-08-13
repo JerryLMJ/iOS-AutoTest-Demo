1cbc9e12f7c64d1489b5a4ddc1f2c1d6f236f996
LiMingjie_Office <limingjie_mail@163.com>
2019-08-13 12:13:09 +0800

    init project

--- iOS-AutoTest-Demo/ViewController.swift
+++ iOS-AutoTest-Demo/ViewController.swift
@@ -6,20 +6,13 @@
 //  Copyright © 2019 LMJ. All rights reserved.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
